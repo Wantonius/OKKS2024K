ed777a21690285da767b42dd776e01f45668f239
Erno Hentonen <erno.hentonen@opiframe.com>
2024-02-23 11:25:50 +0200

    prototype inheritance

--- javascript/09_prototype_inheritance/public/script.js
+++ javascript/09_prototype_inheritance/public/script.js
@@ -1,3 +1,15 @@
+function Universe() {
+	var instance;
+	Universe = function Universe() {
+		return instance;
+	}
+	Universe.prototype = this;
+	instance = new Universe();
+	instance.constructor = Universe;
+	instance.size = 0;
+	instance.bang = "big";
+}
+
 function start() {
 	
 	let HelloWorld = function() {
@@ -19,4 +31,12 @@ function start() {
 	helloInstance2.name = "Test";
 	console.log(helloInstance2.name);
 	console.log(helloInstance2);
+
+	Universe.prototype.nothing = true;
+	let uni1 = new Universe();
+	console.log(uni1);
+	let uni2 = new Universe();
+	uni2.everything = true;
+	console.log(uni1);
+	console.log(uni2);
 }
