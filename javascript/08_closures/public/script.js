5d945cbc05ed3e4cdea9de73ec1387dc938dac9d
Erno Hentonen <erno.hentonen@opiframe.com>
2024-02-23 10:41:41 +0200

    closures

--- javascript/08_closures/public/script.js
+++ javascript/08_closures/public/script.js
@@ -25,3 +25,39 @@ const changeFontSize = function(){
 		}
 	}
 }
+
+const makeCounter = function() {
+	
+	let privateCounter = 0;
+	
+	function changeBy(val) {
+		privateCounter += val;
+	}
+	
+	return {
+		increment:function() {
+			changeBy(1);
+		},
+		decrement:function() {
+			changeBy(-1);
+		},
+		value:function() {
+			return privateCounter;
+		}
+	}
+}
+
+function start() {
+	let counter1 = makeCounter();
+	let counter2 = makeCounter();
+
+	console.log("Counter 1 value",counter1.value());
+	console.log("Counter 2 value",counter2.value());
+	
+	counter1.increment();
+	counter1.increment();
+	counter1.increment();
+
+	console.log("Counter 1 value",counter1.value());
+	console.log("Counter 2 value",counter2.value());
+}
