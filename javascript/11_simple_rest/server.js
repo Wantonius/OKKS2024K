4f093b6c3bcdc15d895b58353cb3c674bd407c38
Erno Hentonen <erno.hentonen@opiframe.com>
2024-02-26 13:09:07 +0200

    rest api

--- javascript/11_simple_rest/server.js
+++ javascript/11_simple_rest/server.js
@@ -53,6 +53,23 @@ app.delete("/api/shopping/:id",function(req,res) {
 	return res.status(200).json({"Message":"Success"})
 })
 
+app.put("/api/shopping/:id", function(req,res) {
+	let tempId = parseInt(req.params.id,10)
+	let item = {
+		"type":req.body.type,
+		"count":req.body.count,
+		"price":req.body.price,
+		"id":tempId
+	}
+	for(let i=0;i<database.length;i++) {
+		if(item.id === database[i].id) {
+			database.splice(i,1,item)
+			return res.status(200).json({"Message":"Success"})
+		}
+	}
+	return res.status(404).json({"Message":"Not found"})
+})
+
 app.listen(3000);
 
 console.log("Running in port 3000");
