e51ed1660eef8e9273077df5c1ed430c7cff7a57
Erno Hentonen <erno.hentonen@opiframe.com>
2024-03-18 12:04:38 +0200

    flask basics

---
+++ python/20_jinja_template_basics/template1.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def get_index():
+    return render_template("index.html",title="Welcome",username="Erno")
+    
+@app.route("/if")
+def if_example():
+    return render_template("if_example.html",title="If example",username="Erno")
+    
+@app.route("/if/<username>")
+def if_username(username):
+    name = escape(username)
+    return render_template("if_example.html",title="If example 2",username=name)
+
+@app.route("/loop")
+def loop():
+    members = ["Jaska","Pekka","Matti"]
+    return render_template("loop.html",title="Loops",members=members)
+    
+app.run("127.0.0.1",port=8080)
