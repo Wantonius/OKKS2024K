5cc772b7083a3b7eae2ea24661f39dbd454cf572
Erno Hentonen <erno.hentonen@opiframe.com>
2024-03-20 09:39:43 +0200

    lists

---
+++ react/06_lists/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import ContactForm from './components/ContactForm';
+
+function App() {
+
+	const [state,setState] = useState({
+		list:[],
+		id:100
+	})
+
+	const addContact = (contact) => {
+		setState((state) => {
+			contact.id = state.id
+			return {
+				list:state.list.concat(contact),
+				id:state.id+1
+			}
+		})
+	}
+	
+	const removeContact = (id) => {
+		setState((state) => {
+			const tempList = state.list.filter(contact => contact.id !== id)
+			return {
+				...state,
+				list:tempList
+			}
+		})
+	}
+
+	return (
+		<>
+			<ContactForm addContact={addContact}/>
+		</>
+	)
+}
+
+export default App
