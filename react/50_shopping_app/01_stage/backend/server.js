6f9b7136ec8e58853870e037026fdac138d3a18b
Erno Hentonen <erno.hentonen@opiframe.com>
2024-03-25 09:39:15 +0200

    shopping app 1

---
+++ react/50_shopping_app/01_stage/backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const shoppingRoute = require('./routes/shoppingroute');
+
+let app = express();
+
+app.use(express.json());
+
+const mongo_url = process.env.MONGODB_URL;
+const mongo_user = process.env.MONGODB_USER;
+const mongo_password = process.env.MONGODB_PASSWORD;
+
+const url = "mongodb+srv://"+mongo_user+":"+mongo_password+"@"+mongo_url+"/okksshopping?retryWrites=true&w=majority&appName=testiklusteri"
+
+mongoose.connect(url).then(
+	() => console.log("Connected to MongoDB"),
+	(err) => console.log("Failed to connect to MongoDB. Reason",err)
+)
+
+app.use("/api",shoppingRoute);
+
+app.listen(3000);
+
+console.log("Running in port 3000");
