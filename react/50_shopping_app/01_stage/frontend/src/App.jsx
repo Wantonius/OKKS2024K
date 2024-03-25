e21c746d945086d576d1d7ed863bdf9c9f2c30ad
Erno Hentonen <erno.hentonen@opiframe.com>
2024-03-25 10:59:24 +0200

    shopping app 1

---
+++ react/50_shopping_app/01_stage/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import useAction from './hooks/useAction';
+
+
+function App() {
+
+	const {state,addItem,removeItem,editItem} = useAction();
+
+	return (
+		<>
+
+		</>
+	)
+}
+
+export default App
