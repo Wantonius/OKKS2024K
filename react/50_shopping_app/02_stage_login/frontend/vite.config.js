2f8c872fb3a454a4bcc4196ce2e1bf5351ac7278
Erno Hentonen <erno.hentonen@opiframe.com>
2024-03-26 13:18:23 +0200

    login stage

--- react/50_shopping_app/02_stage_login/frontend/vite.config.js
+++ react/50_shopping_app/02_stage_login/frontend/vite.config.js
@@ -10,7 +10,22 @@ export default defineConfig({
 			  target:"http://localhost:3000",
 			  changeOrigin:true,
 			  secure:false
-		  }
+		  },
+		  "/register":{
+			  target:"http://localhost:3000",
+			  changeOrigin:true,
+			  secure:false
+		  },
+		  "/login":{
+			  target:"http://localhost:3000",
+			  changeOrigin:true,
+			  secure:false
+		  },
+		  "/logout":{
+			  target:"http://localhost:3000",
+			  changeOrigin:true,
+			  secure:false
+		  }		  
 	  }
   }
 })
