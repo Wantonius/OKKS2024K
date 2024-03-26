309a17123e83ce4b71d4ad605bbfd9f3075d3ead
Erno Hentonen <erno.hentonen@opiframe.com>
2024-03-26 10:26:25 +0200

    login stage

--- react/50_shopping_app/02_stage_login/backend/server.js
+++ react/50_shopping_app/02_stage_login/backend/server.js
@@ -25,6 +25,10 @@ mongoose.connect(url).then(
 
 const time_to_live_diff = 3600000
 
+createToken = () => {
+	let token = crypto.randomBytes(64);
+	return token.toString("hex");
+}
 
 //LOGIN API
 
@@ -58,6 +62,49 @@ app.post("/register",function(req,res) {
 		})
 	})
 })
+
+app.post("/login",function(req,res) {
+	if(!req.body) {
+		return res.status(400).json({"Message":"Bad Request"})
+	}
+	if(!req.body.username || !req.body.password) {
+		return res.status(400).json({"Message":"Bad Request"})
+	}
+	if(req.body.username.length < 4 || req.body.password.length < 8) {
+		return res.status(400).json({"Message":"Bad Request"})
+	}
+	userModel.findOne({"username":req.body.username}).then(function(user) {
+		if(!user) {
+			return res.status(401).json({"Message":"Unauthorized"})
+		}
+		bcrypt.compare(req.body.password,user.password,function(err,success) {
+			if(err) {
+				console.log(err);
+				return res.status(500).json({"Message":"Internal server error"})
+			}
+			if(!success) {
+				return res.status(401).json({"Message":"Unauthorized"})
+			}
+			let token = createToken();
+			let now = Date.now();
+			let session = new sessionModel({
+				"user":req.body.username,
+				"ttl":now+time_to_live_diff,
+				"token":token
+			})
+			session.save().then(function() {
+				return res.status(200).json({"token":token})
+			}).catch(function(err) {
+				console.log(err);
+				return res.status(500).json({"Message":"Internal server error"})
+			})
+		})
+	}).catch(function(err) {
+		console.log(err);
+		return res.status(500).json({"Message":"Internal server error"})
+	})
+})
+
 app.use("/api",shoppingRoute);
 
 app.listen(3000);
