fe99a2a1ef8793db43580b11b1da4c8731465430
Erno Hentonen <erno.hentonen@opiframe.com>
2024-03-27 10:00:35 +0200

    redux shopping

---
+++ react/12_redux_test_2/src/App.jsx
@@ -0,0 +1,15 @@
+import ContactForm from './components/ContactForm';
+import ContactList from './components/ContactList';
+
+function App() {
+
+  return (
+    <>
+		<ContactForm/>
+		<hr/>
+		<ContactList/>
+    </>
+  )
+}
+
+export default App
