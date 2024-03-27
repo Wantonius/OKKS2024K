5d55df846d05a8c411760f1a7ad0846f7287684f
Erno Hentonen <erno.hentonen@opiframe.com>
2024-03-27 12:02:05 +0200

    redux shopping

--- react/50_shopping_app/03_stage_redux/frontend/src/actions/loginActions.jsx
+++ react/50_shopping_app/03_stage_redux/frontend/src/actions/loginActions.jsx
@@ -16,12 +16,38 @@ export const register = (user) => {
 	}
 }
 
+export const login = (user) => {
+	return (dispatch) => {
+		let request = {
+			"method":"POST",
+			"headers":{
+				"Content-Type":"application/json"
+			},
+			"body":JSON.stringify(user)
+		}
+		dispatch(setUser(user.username));
+		handleLogin(request,"/login","login",dispatch)
+	}
+}
+
+export const logout = (token) => {
+	return (dispatch) => {
+		let request = {
+			"method":"POST",
+			"headers":{
+				"token":token
+			}
+		}
+		handleLogin(request,"/logout","logout",dispatch)
+	}
+}
+
 const handleLogin = async (request,url,act,dispatch) => {
 	dispatch(loading());
 	const response = await fetch(url,request);
 	dispatch(stopLoading());
 	if(!response) {
-		//dispatch logoutFailed
+		dispatch(logoutFailed("Server never responded. Resetting"))
 		return;
 	}
 	if(response.ok) {
@@ -29,6 +55,17 @@ const handleLogin = async (request,url,act,dispatch) => {
 			case "register":
 				dispatch(registerSuccess());
 				return;
+			case "login":
+				const data = await response.json();
+				if(!data) {
+					dispatch(loginFailed("Failed to parse login information. Try again later"));
+					return;
+				}
+				dispatch(loginSuccess(data.token));
+				//dispatch getlist
+				return;
+			case "logout":
+				dispatch(logoutSuccess());
 			default:
 				return;
 		}
@@ -41,6 +78,12 @@ const handleLogin = async (request,url,act,dispatch) => {
 					return;
 				}
 				dispatch(registerFailed("Register failed."+errorMessage));
+				return;
+			case "login":
+				dispatch(loginFailed("Login failed."+errorMessage));
+				return;
+			case "logout":
+				dispatch(logoutFailed("Server responded with an error. Logging you out."));
 				return;
 			default:
 				return;
@@ -72,5 +115,39 @@ export const registerFailed = (error) => {
 	return {
 		type:actionConstants.REGISTER_FAILED,
 		error:error
+	}
+}
+
+const loginSuccess = (token) => {
+	return {
+		type:actionConstants.LOGIN_SUCCESS,
+		token:token
+	}
+}
+
+const loginFailed = (error) => {
+	return {
+		type:actionConstants.LOGIN_FAILED,
+		error:error
+	}
+}
+
+const logoutSuccess = () => {
+	return {
+		type:actionConstants.LOGOUT_SUCCESS
+	}
+}
+
+export const logoutFailed = (error) => {
+	return {
+		type:actionConstants.LOGOUT_FAILED,
+		error:error
+	}
+}
+
+const setUser = (user) => {
+	return {
+		type:actionConstants.SET_USERNAME,
+		user:user
 	}
 }
--- react/50_shopping_app/03_stage_redux/frontend/src/components/Navbar.jsx
+++ react/50_shopping_app/03_stage_redux/frontend/src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import {Link} from 'react-router-dom';
+import {useDispatch,useSelector} from 'react-redux';
+import {logout} from '../actions/loginActions';
 
 const Navbar = (props) => {
-	if(props.isLogged) {
+	
+	const selector = (state) => {
+		return {
+			isLogged:state.login.isLogged,
+			token:state.login.token,
+			user:state.login.user
+		}
+	}
+	const state = useSelector(selector);
+	const dispatch = useDispatch();
+	
+	if(state.isLogged) {
 		return(
 			<nav className="navbar navbar-expand-lg navbar-light bg-light">
 				<p className="navbar-brand" style={{marginLeft:10}}>Shopping App</p>
@@ -13,10 +26,10 @@ const Navbar = (props) => {
 						<Link to="/form" className="nav-link">Add new item</Link>
 					</li>
 					<li className="nav-item" style={{marginLeft:10}}>
-						<p className="nav-link" style={{color:"blue"}}>Logged in as {props.user}</p>
+						<p className="nav-link" style={{color:"blue"}}>Logged in as {state.user}</p>
 					</li>
 					<li className="nav-item" style={{marginLeft:10}}>
-						<Link to="/" className="nav-link" onClick={props.logout}>Logout</Link>
+						<Link to="/" className="nav-link" onClick={() => dispatch(logout(state.token))}>Logout</Link>
 					</li>						
 				</ul>
 			</nav>
