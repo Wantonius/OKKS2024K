463131d847e3df048568465eed7e1cbea786ae98
Erno Hentonen <erno.hentonen@opiframe.com>
2024-04-02 14:37:50 +0300

    number guess game context

--- react/15_number_guess_game_with_context/src/context/GameProvider.jsx
+++ react/15_number_guess_game_with_context/src/context/GameProvider.jsx
@@ -37,6 +37,91 @@ const GameProvider = (props) => {
 		})
 		navigate("/game")
 	}
+	
+	const guess = (guess) => {
+		if(state.targetNumber === 0) {
+			setState({
+				targetNumber:0,
+				playerName:"",
+				noOfGuesses:0,
+				minGuess:1,
+				maxGuess:100,
+				message:""
+			})
+			navigate("/")
+		}
+		if(isNaN(guess)) {
+			let message = "Please enter a NUMBER between "+state.minGuess+" and "+state.maxGuess+".";
+			setState((state) => {
+				return {
+					...state,
+					message:message
+				}
+			})
+			return;
+		}
+		let tempGuess = parseInt(guess,10);
+		if(tempGuess < state.minGuess || tempGuess > state.maxGuess) {
+			let message = "Please enter a number between "+state.minGuess+" and "+state.maxGuess+".";
+			setState((state) => {
+				return {
+					...state,
+					message:message
+				}
+			})
+			return;			
+		}
+		if(tempGuess < state.targetNumber && tempGuess >= state.minGuess) {
+			let message = "You guessed too low. Guess between "+guess+" and "+state.maxGuess+".";
+			setState((state) => {
+				return {
+					...state,
+					noOfGuesses:state.noOfGuesses+1,
+					minGuess:guess,
+					message:message
+				}
+			})
+			return;
+		}
+		if(tempGuess > state.targetNumber && tempGuess <= state.maxGuess) {
+			let message = "You guessed too high. Guess between "+state.minGuess+" and "+guess+".";
+			setState((state) => {
+				return {
+					...state,
+					noOfGuesses:state.noOfGuesses+1,
+					maxGuess:guess,
+					message:message
+				}
+			})
+			return;
+		}
+		if(tempGuess === state.targetNumber) {
+			let noOfGuesses = state.noOfGuesses +1;
+			alert("Congrats "+state.playerName+"! You won with "+noOfGuesses+" guesses.")
+			setState({
+				playerName:"",
+				targetNumber:0,
+				minGuess:1,
+				maxGuess:100,
+				noOfGuesses:0,
+				message:""
+			})
+			navigate("/")
+			return;
+		}
+		console.log("Should not come here!")
+	}
+
+	return(
+		<GameContext.Provider value={{
+			startGame:startGame,
+			guess:guess,
+			message:state.message
+		}}>
+			{props.children}
+		</GameContext.Provider>
+		)
+
 }
 
 export default GameProvider;
--- react/15_number_guess_game_with_context/src/main.jsx
+++ react/15_number_guess_game_with_context/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {BrowserRouter} from 'react-router-dom';
+import GameProvider from './context/GameProvider';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <BrowserRouter>
+  <GameProvider>
     <App />
+  </GameProvider>
+  </BrowserRouter>
   </React.StrictMode>,
 )
